3d23db76e2cc55e668c4567d70b835f213a28df9
John <johntaykimani@gmail.com>
2024-08-21 13:00:40 +0300

    resourses updated

--- server/resources.py
+++ server/resources.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request, abort
-from flask_restful import Api, Resource
+from flask_restful import Resource
 from flask_login import LoginManager, login_user, logout_user, login_required
 from models import db, Country, HsCode, Product, ExportTable, ImportTable, TaxTable, User
 
-
+app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'loginresource'
